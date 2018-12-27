1cb661294b4d2f34aa81727d81f589f2e8e54da4
nishisaxenatct <45178342+nishisaxenatct@users.noreply.github.com>
2018-12-26 20:07:31 -0600

    Adding new file

---
+++ java-programs/src/main/java/com/product/java/core/PrintName.java
@@ -0,0 +1,12 @@
+package com.product.java.core;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Printing Name : Anuj");
+
+	}
+
+}
